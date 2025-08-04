04d48debb420550eb2038d9981f3fbf786f5d9e5
JoshLyu <47506371+JoshLyu@users.noreply.github.com>
2025-08-04 17:23:33 +0800

    update

--- intro.java
+++ intro.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("Hello, I am Josh");
+        System.out.println("Hello, I am Josh on Aug 4 2025");
     }
 }
