8d3edb8f221ba47f239bc52c045cd507244659cd
Phoebechen131 <xinyuec131@gmail.com>
2025-08-06 16:55:41 +0800

    Update intro.java

--- intro.java
+++ intro.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("Hello, I am Josh on Aug 4 2025");
+        System.out.println("Hello, I am Josh on Aug 6 2025");
+        System.out.println("Hello, I am Phoebe on  Aug 6 2025");
     }
-}
