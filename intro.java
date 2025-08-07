b87bd18d6de67eed29b11072f49ae701dbe5b0d7
JoshLyu <47506371+JoshLyu@users.noreply.github.com>
2025-08-07 17:44:32 +0800

    Update intro.java

--- intro.java
+++ intro.java
@@ -1,6 +1,31 @@
 public class intro {
+    static String name3 = "Emma Y";
+
     public static void main(String[] args)
     {
-        System.out.println("Hello, I am Josh on Aug 4 2025");
+        String name = "Phoebe";
+        String name1 = "Emma S";
+        String name4 = "Kevin";
+
+        System.out.println("part 1");
+    
+        System.out.println("Hello, I am "+ name3 +" on Aug 4 2025");
+        System.out.println("Hello, I am "+ name +" on Aug 4 2025");
+        System.out.println("Hello, I am "+ name +" on Aug 4 2025");
+        System.out.println("Hello, I am "+ name +" on Aug 4 2025");
+        System.out.println("Hello, I am "+ name +" on Aug 4 2025");
+        
+        if(name == "Phoebe"){
+            System.out.println("part 2");
+            int Sum = 0;
+            //= means is 
+            for(int i = 50; i<151; i=i+1){
+                    Sum = Sum + i;
+                    System.out.println("add 1 to 100:"+ Sum );
+            }
+        }
+
+
     }
+    String name4 = "Charles";
 }
