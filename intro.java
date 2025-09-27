bdfc37afd5007942c445878b22d2ff96814817c9
JoshLyu <47506371+JoshLyu@users.noreply.github.com>
2025-09-27 09:30:59 +0800

    something new

--- intro.java
+++ intro.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 public class intro {
     public static void main(String[] args)
     {
-        System.out.println("Hello, I am Emma on Aug 6 2025");
+        Scanner scanner = new Scanner(System.in); // 从控制台读取
+        String name = scanner.nextLine();
+        System.out.println("Hello, I am "+name+" on Aug 6 2025");
         System.out.println("Hello, I am Emma on Aug 6 2025");
     }
 }
