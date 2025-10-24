507711313bb685b97e108fba63d7a94effa7aa86
Yuyue777 <YZ2205@msmnyc.edu>
2025-10-24 18:53:16 -0400

    w

--- intro.java
+++ intro.java
@@ -3,18 +3,25 @@ public class intro {
     public static void main(String[] args) {
         Scanner scanner1 = new Scanner(System.in); // 从控制台读取
 
-
+        String[] names  = new String[5];
 
         for (int i = 0; i < 5; i++) {
-            System.out.println("input the name" + (i + 1) + ": ");
+            System.out.println("input the firstname" + (i + 1) + ": ");
             String name = scanner1.nextLine();
-            System.out.println(" I am " + name + " on Oct 4 2025");
+            names[i]=name;
+            System.out.println(" Hello I am " + names[i]+ " on Oct 4 2025");
+        }
 
-            if (i == 2) {
-                System.out.println("Hello, I am " + name + " on Oct 4 2025");
-            }
-
-
+        for (int i = 0; i < 5; i++) {
+            System.out.println("input the Lastname" + (i + 1) + ": ");
+            String name = scanner1.nextLine();
+            names[i]=names[i]+" "+name;
+            System.out.println(" Hello I am " + names[i]+ " on Oct 4 2025");
+        }
+        int i = 0;
+        while (i < 5) {
+            System.out.println(" Hello I am " + names[i]+ " on Oct 4 2025");
+         i++;
         }
     }
     }
